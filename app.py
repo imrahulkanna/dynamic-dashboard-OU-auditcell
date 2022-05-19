d1e173e33e2302b597dba2c19e34909489f4d649
Rahul Kanna <67679039+RahulKanna05@users.noreply.github.com>
2022-05-19 14:03:23 +0530

    automatic rendering of clg prgms and their count

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, session
-import MySQLdb
+# import MySQLdb
 from flask_mysqldb import MySQL
 
 app = Flask(__name__)
@@ -20,4 +20,13 @@ def login():
 
 @app.route('/home',methods=['POST','GET'])
 def home():
-    return render_template('home.html')
+    cursor = db.connection.cursor()
+    cursor2 = db.connection.cursor()
+    cursor.execute('SELECT program, count(distinct clgcode) FROM clglist group by program')
+    details = cursor.fetchall()
+    sno=len(details)
+
+    cursor2.execute('SELECT COUNT(distinct clgcode) from clglist')
+    clgCount=cursor2.fetchall()[0][0]
+
+    return render_template('home.html',sno=sno,details=details,clgCount=clgCount)
