f9465609b02aa4432e9241b4195a30d2b5429011
thefarazxr <farazuddinmohammed05@gmail.com>
2022-05-23 15:59:53 +0530

    added password meta

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '12345678'
+# app.config['MYSQL_PASSWORD'] = '' # for no password
+app.config['MYSQL_PASSWORD'] = '12345678' # for password
 app.config['MYSQL_DB'] = 'auditcell'
 
 db = MySQL(app)
