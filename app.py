4cc1ce2e709993fbf9dbfda459ca07d09c4a9048
mtanmayi <tanmayi18m@gmail.com>
2022-07-26 15:14:10 +0530

    db added

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ app.secret_key = 'auditcell'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-# app.config['MYSQL_PASSWORD'] = '' # for no password
-app.config['MYSQL_PASSWORD'] = '12345678' # for password
+app.config['MYSQL_PASSWORD'] = '' # for no password
+#app.config['MYSQL_PASSWORD'] = '12345678' # for password
 app.config['MYSQL_DB'] = 'auditcell'
 
 db = MySQL(app)
